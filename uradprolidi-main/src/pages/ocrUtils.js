c1e97584c5c86c7760d0e72d08c9a63822120ceb
Jiri Till <jirik.till@gmail.com>
2025-07-16 11:03:31 +0200

    Delete uradprolidi-main/src/pages/ocrUtils.js

--- uradprolidi-main/src/pages/ocrUtils.js
+++
@@ -1,6 +0,0 @@
-import Tesseract from 'tesseract.js';
-
-export async function recognizeTextFromImage(imageFile) {
-  const { data: { text } } = await Tesseract.recognize(imageFile, 'eng');
-  return text;
-}
