b43f0da59ed27f8c894304d130ed4049817b8a28
Jiri Till <jirik.till@gmail.com>
2025-07-24 14:27:58 +0200

    Update vite.config.js

--- uradprolidi-main/vite.config.js
+++ uradprolidi-main/vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     target: 'es2022',
-  }
-})
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
+  },
+});
